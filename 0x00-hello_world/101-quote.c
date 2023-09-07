bdf5eaa83a7504887aec3175a9f655ec20920f35
dawe <dawittamiru014@gmail.com>
2023-09-07 20:54:27 +0300

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-const char *m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char *m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 ssize_t len = 0;
 while (m[len] != '\0')
 {
