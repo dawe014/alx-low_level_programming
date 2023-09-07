bd4c0a55fb2382c7be111f13e2ea450c00e90bd1
dawe <dawittamiru014@gmail.com>
2023-09-07 20:41:55 +0300

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,21 @@
-#include<unistd.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 1 (success)
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 1 (Error code)
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, sizeof(message) - 1);
+const char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+ssize_t len = 0;
+while (message[len] != '\0')
+{
+len++;
+}
+if (write(STDERR_FILENO, message, len) != len)
+{
+return (1);
+}
 return (1);
 }
