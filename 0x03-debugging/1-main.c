bb726eed5ca9f2213b4b7e7524f22b28180ebe17
dawe <dawittamiru014@gmail.com>
2023-09-15 19:15:59 +0300

    update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,6 @@
  * Return: 0
 */
 
-
 int main(void)
 {
 	int i;
@@ -16,7 +15,7 @@ int main(void)
 /*	
 *	while (i < 10)
 *	{
-*	      putchar(i);
+*		putchar(i);
 *	}
 */	
 	printf("Infinite loop avoided! \\o/\n");
