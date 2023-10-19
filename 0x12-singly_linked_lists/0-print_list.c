ad5b97caaaf3f8fff82a108115c5ce643a0e3e21
dawe014 <dawittamiru014@gmail.com>
2023-10-19 17:10:20 +0300

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * print_list - Prints all element of linked list
- * @h: A pointer to the head of linked list
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
  *
- * Return: The number of nodes in the list
-*/
+ * Return: the number of nodes printed
+ */
 size_t print_list(const list_t *h)
 {
-	const list_t *ptr = h;
-	size_t i = 0;
+	size_t n = 0;
 
-	while (ptr != NULL)
+	while (h)
 	{
-        if(h->str == NULL)
-        {
-             printf("[0] (nil)\n");
-        }
-		printf("[%u] %s\n", ptr->len, ptr->str);
-		ptr = ptr->next;
-		i++;
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
 	}
-	return (i);
+
+	return (n);
 }
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-/**
- * my_constructor - Constructor function to print a message before main.
- *
- * This function is marked with a GCC-specific constructor attribute,
- * which ensures it is executed before the main function.
- */
-void my_constructor(void) __attribute__((constructor));
+#include <stdio.h>
 
-void my_constructor(void)
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
