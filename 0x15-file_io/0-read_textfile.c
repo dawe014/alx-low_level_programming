24ead23fd0cca9ce16dab2b8a3ffe444e6cf03ca
dawe014 <dawittamiru014@gmail.com>
2023-10-30 22:52:26 +0300

    c file operation

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,38 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- * O/w - the actual number of bytes the function can read and print.
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t Open, Read, Write;
-	char *Buffer;
+	int fdisc;
+	int i, y;
+	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-
-	Buffer = malloc(sizeof(char) * letters);
-	if (Buffer == NULL)
+	fdisc = open(filename, O_RDONLY);
+	if (fdisc < 0)
 		return (0);
-
-	Open = open(filename, O_RDONLY);
-	Read = read(Open, Buffer, letters);
-	Write = write(STDOUT_FILENO, Buffer, Read);
-
-	if (Open == -1 || Read == -1 || Write == -1 || Write != Read)
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+	i = read(fdisc, buffer, letters);
+	if (i < 0)
 	{
-		free(Buffer);
+		free(buffer);
 		return (0);
 	}
-
-	free(Buffer);
-	close(Open);
-
-	return (Write);
+	buffer[i] = '\0';
+	close(fdisc);
+	y = write(STDOUT_FILENO, buffer, i);
+	if (y < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	return (y);
 }
